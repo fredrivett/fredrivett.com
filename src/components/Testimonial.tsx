f7cc921add3eda4c7d23dba5d06bb7302323abfa
Fred Rivett <fred@fredrivett.com>
2023-02-21 17:05:44 +0000

    tweak testimonial quote style on dark mode to be slightly darker

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -11,11 +11,11 @@ const Testimonial = ({ className, children, cite }: Props) => (
     className={`relative ml-0 mb-8 print:-mt-4 text-sm ${className ?? ""}`}
   >
     <div className="p-4 print:pb-0 indent-4 bg-gray-200 dark:bg-gray-900 rounded-xl">
-      <span className="absolute top-1 left-0 md:top-0 md:-left-1 text-gray-500 fs-1 font-header">
+      <span className="absolute top-1 left-0 md:top-0 md:-left-1 text-gray-500 dark:text-gray-700 fs-1 font-header">
         “
       </span>
       {children}
-      <span className="absolute -mt-3 md:-mt-4 -ml-4 text-gray-500 fs-1 font-header">
+      <span className="absolute -mt-3 md:-mt-4 -ml-4 text-gray-500 dark:text-gray-700 fs-1 font-header">
         ”
       </span>
     </div>
