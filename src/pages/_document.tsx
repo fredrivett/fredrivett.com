d3da73714330e2dbefec305a2cd7b74597032231
Fred Rivett <fred@fredrivett.com>
2025-09-10 14:45:27 +0100

    make light text body darker (was too light)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -23,7 +23,7 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
         </Head>
-        <body className="text-gray-700 dark:text-gray-400 dark:bg-gray-950">
+        <body className="text-gray-800 dark:text-gray-400 dark:bg-gray-950">
           <Main />
           <NextScript />
         </body>
